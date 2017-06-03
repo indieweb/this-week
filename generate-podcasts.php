d850e5e96f52491bfff6696e51efb82b930dc934
Aaron Parecki <aaron@parecki.com>
2017-06-03 15:56:03 +0000

    add list of recent podcasts

---
+++ generate-podcasts.php
@@ -0,0 +1,43 @@
+<?php
+	
+$sources = ['https://huffduffer.com/tags/indieweb'];
+
+$submissions = [];
+
+foreach($sources as $u) {
+	$feed = parse_page($u);
+	$entries = $feed['items'][0]['children'];
+	foreach($entries as $entry) {
+  	
+  	# huffduffer markup has published date in the p-author h-card, oops
+  	if(isset($entry['properties']['published']))
+  	  $published = $entry['properties']['published'][0];
+    else
+      $published = $entry['properties']['author'][0]['properties']['published'][0];
+      
+		if(strtotime($published) >= $startDate) {
+			$name = $entry['properties']['name'][0];
+			$url = $entry['properties']['url'][0];
+			$published = new DateTime($published);
+
+      $content = $entry['properties']['content'][0]['html'];
+			
+			ob_start();
+			echo '<div style="margin-bottom: 1em;" class="h-entry">';
+		    if($name)
+					echo '<div style="font-size:1.3em;font-weight:bold;"><a href="'.$url.'" class="u-url p-name">'.$name.'</a></div>';
+				else
+				  echo '';
+				if($content) {
+					echo '<div class="e-content">'.$content.'</div>';
+				}
+			echo '</div>'."\n";
+			$submissions[] = ob_get_clean();	
+		}
+	}
+}
+
+if(count($submissions)) {
+	echo '<h2 id="podcasts">Podcasts</h2>';
+	echo implode("\n", $submissions);
+}
